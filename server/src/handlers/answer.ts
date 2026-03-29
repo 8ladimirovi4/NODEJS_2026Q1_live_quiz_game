596c58ce32fdf57adf7569e5eac301947a75abb6
Vladimir_Leonov <2227331.spb@gmail.com>
2026-03-29 19:09:26 +0300

    feat: implement answer handler

--- server/src/handlers/answer.ts
+++ server/src/handlers/answer.ts
@@ -1,6 +1,91 @@
 import type WebSocket from 'ws';
+import { gamesById, userBySocket } from '../state.js';
+import { send } from '../protocol.js';
+import type { AnswerData, Game } from '../types.js';
+import { finalizeCurrentQuestion } from '../game/round.js';
 
-export function handleAnswer(ws: WebSocket, data: unknown): void {
-  void ws;
-  void data;
+export function handleAnswer(ws: WebSocket, data: AnswerData): void {
+  const userId = userBySocket.get(ws);
+  const game = gamesById.get(data.gameId);
+  if (!game) {
+    send(ws, 'error', {
+      message: 'Game error',
+    });
+    return;
+  }
+
+  const isValidData = (
+    g: Game | undefined,
+    uid: string | undefined,
+  ): boolean => {
+    if (!uid) {
+      send(ws, 'error', {
+        message: 'User error',
+      });
+      return false;
+    }
+
+    if (!g) {
+      send(ws, 'error', {
+        message: 'Game error',
+      });
+      return false;
+    }
+
+    if (data.questionIndex !== g.currentQuestion) {
+      return false;
+    }
+    if (
+      !Number.isInteger(data.answerIndex) ||
+      data.answerIndex < 0 ||
+      data.answerIndex > 3
+    ) {
+      return false;
+    }
+    if (uid === g.hostId) {
+      return false;
+    }
+    return true;
+  };
+
+  if (!isValidData(game, userId)) {
+    send(ws, 'error', {
+      message: 'Data error',
+    });
+    return;
+  }
+
+  //time limit over
+  const playerAnsweredTimestamp = Date.now();
+  const question = game.questions[game.currentQuestion];
+  const qStartTime = game.questionStartTime;
+  if (!question || !qStartTime) {
+    console.error('Something gose wrong');
+    return;
+  }
+  if (playerAnsweredTimestamp > qStartTime + question.timeLimitSec * 1000) {
+    send(ws, 'error', {
+      message: 'Time is over',
+    });
+    return;
+  }
+
+  //set answer to db
+  for (const p of game.players) {
+    if (p.index === userId) {
+      const points = 0; //TBD from points
+      p.score += points;
+      p.hasAnswered = true;
+      p.answerTime = playerAnsweredTimestamp - qStartTime;
+      p.answeredCorrectly = data.answerIndex === question.correctIndex;
+    }
+  }
+
+  send(ws, 'answer_accepted', { questionIndex: game.currentQuestion });
+
+  const isAllanswered = game.players.every((p) => p.hasAnswered);
+
+  if (isAllanswered) {
+    finalizeCurrentQuestion(game);
+  }
 }
